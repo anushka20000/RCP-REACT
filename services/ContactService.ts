550fbb0c51851be1605d8c1c307df1edc5d65720
anushka20000 <96882389+anushka20000@users.noreply.github.com>
2025-07-07 11:34:50 +0530

    Delete services directory

--- services/ContactService.ts
+++
@@ -1,9 +0,0 @@
-import api from "./apiClient";
-import { ContactServiceResponse } from "../types/ContactType";
-
-const addContact = async (data) => {
-    const {data:response} = await api.post("admin/contact/store", data);
-    const result: ContactServiceResponse = response;
-    return result
-}
-export {addContact}
